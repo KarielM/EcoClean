774f2c8465ab1be1446bfcc33f2c4a50e2ae45b7
kwicker22 <kwicker2021@gmail.com>
2024-08-05 15:18:50 -0500

    added templates

--- EcoClean/App/views.py
+++ EcoClean/App/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def base(request):
+    return render(request, "base.html")
+
+
+def home(request):
+    return render(request, "home.html")
