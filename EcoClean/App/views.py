bb6de95704aa35954a8f44e2ddfd9e619328e8c5
kwicker22 <kwicker2021@gmail.com>
2024-08-05 16:09:33 -0500

    templates and paths complete

--- EcoClean/App/views.py
+++ EcoClean/App/views.py
@@ -4,8 +4,21 @@ from django.shortcuts import render, redirect
 
 
 def base(request):
+
     return render(request, "base.html")
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def aboutUs(request):
+    return render(request, "aboutUs.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
--- EcoClean/config/urls.py
+++ EcoClean/config/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from App.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", )
+    path("", home, name="home"),
+    path("/contact", contact, name ="contact"),
+    path("/about", aboutUs, name="about"),
+    path("/services", services, name="services")
 ]
