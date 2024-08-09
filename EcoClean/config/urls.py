c8b4a9a8bf57c53434faf7d0a464600147f63cd6
kwicker22 <kwicker2021@gmail.com>
2024-08-08 20:58:49 -0500

    changes

--- EcoClean/config/urls.py
+++ EcoClean/config/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from App.views import *
@@ -6,10 +5,9 @@ from App.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
-    path("contact/", contact, name ="contact"),
+    path("contact/", contact, name="contact"),
     path("about/", aboutUs, name="about"),
     path("services/", services, name="services"),
-    path('add-zip-code/', add_zip_code, name='add_zip_code'),
-    path('check-zip-code/', check_zip_code, name='check_zip_code'),
-    path('contact_us/', contactFormView, name = 'contactUs')
+    path("check-zip-code/", validate_zipcode, name="check_zip_code"),
+    path("contact_us/", contactFormView, name="contactUs"),
 ]
