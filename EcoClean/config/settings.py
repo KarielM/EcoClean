a3329a6c3aca578e76713e7a442490a004d492dc
KarielM <mosley.kariel@gmail.com>
2024-08-17 14:47:08 -0500

    changed booking form state and time range

--- EcoClean/config/settings.py
+++ EcoClean/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j_qi2=9=tu!+_b@^k3_wx-@ykz12$q)ig+-7&7w_kpn+hhpday"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
@@ -33,7 +33,7 @@ EMAIL_HOST_USER = "ecocleanbusiness2024@gmail.com"
 EMAIL_HOST_PASSWORD = "fyho gsri ieig ssmu"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-ALLOWED_HOSTS = ['*', 'ecoclean-production.up.railway.app']
+ALLOWED_HOSTS = ["*", "ecoclean-production.up.railway.app"]
 
 
 # Application definition
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -126,12 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
